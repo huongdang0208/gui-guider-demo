c99034e165f4dd3ec6c1ba9490450ca9bf49ea13
ducky-coding <dangthithuhuong02082002@gmail.com>
2024-05-26 11:23:35 +0700

    add demos & examples

---
+++ generated/build/CMakeFiles/gui_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gui_demo.dir/main.c.o"
+  "CMakeFiles/gui_demo.dir/main.c.o.d"
+  "gui_demo"
+  "gui_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gui_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
